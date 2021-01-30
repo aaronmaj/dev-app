ada67c39f312546fdb02ac02c83007ec2e4a0d09
Aaron MAJAMBO <aaronmajb@gmail.com>
2021-01-30 15:59:01 +0200

    Adding EnableSwagger to PaymentAppApplication

--- payment-service/src/main/java/com/dev/app/PaymentAppApplication.java
+++ payment-service/src/main/java/com/dev/app/PaymentAppApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.core.env.AbstractEnvironment;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableSwagger2
 public class PaymentAppApplication {
 	//you can use -Dspring.profiles.active=dev when starting app instead of hardcoding it
 	//System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "dev");

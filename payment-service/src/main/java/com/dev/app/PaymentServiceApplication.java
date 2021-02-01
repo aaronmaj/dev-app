0a3d416d482ab4513fd9a2511973310a14211c7d
Aaron MAJAMBO <aaronmajb@gmail.com>
2021-02-02 01:30:32 +0200

    End of day commit

--- payment-service/src/main/java/com/dev/app/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/dev/app/PaymentServiceApplication.java
@@ -1,11 +1,16 @@
 package com.dev.app;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.env.AbstractEnvironment;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Properties;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
@@ -16,7 +21,11 @@ public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(PaymentServiceApplication.class);
+		app.setApplicationStartup(new BufferingApplicationStartup(2048));
 		app.setLazyInitialization(false);
+		//..add other behaviours
+		app.setBannerMode(Banner.Mode.OFF);
+		app.setAddCommandLineProperties(false); //to disable command line properties
 		app.run(args);
 
 	}

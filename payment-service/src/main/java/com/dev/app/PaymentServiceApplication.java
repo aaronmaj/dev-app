7192998b6848035b04c3c9f778930f4e40aed705
Aaron MAJAMBO <aaronmajb@gmail.com>
2021-02-01 22:39:13 +0200

    Renaming PaymentAppApplication.java to PaymentServiceApplication.java

--- payment-service/src/main/java/com/dev/app/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/dev/app/PaymentServiceApplication.java
@@ -5,18 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.core.env.AbstractEnvironment;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableSwagger2
-public class PaymentAppApplication {
+public class PaymentServiceApplication {
 	//you can use -Dspring.profiles.active=dev when starting app instead of hardcoding it
 	//System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "dev");
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaymentAppApplication.class, args);
+		SpringApplication.run(PaymentServiceApplication.class, args);
 	}
 
 	@Bean

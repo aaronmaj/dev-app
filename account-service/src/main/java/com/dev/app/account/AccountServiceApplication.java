6287391678c01369a334f2f79ecb0361605fbefe
Aaron MAJAMBO <aaronmajb@gmail.com>
2021-02-16 00:52:55 +0200

    Refactored microservices to meet architecture patterns

--- account-service/src/main/java/com/dev/app/account/AccountServiceApplication.java
+++ account-service/src/main/java/com/dev/app/account/AccountServiceApplication.java
@@ -4,10 +4,12 @@ import com.dev.app.common.resources.config.MapperConfig;
 import com.dev.app.common.resources.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Import;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 @EnableSwagger2
 @Import({SwaggerConfig.class, MapperConfig.class})
 public class AccountServiceApplication {
